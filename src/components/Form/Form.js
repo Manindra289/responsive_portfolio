94c4d4abab6205d9e1cc1a39b0499c56f66e83d8
Khandyana Manindra <manindrakhandyana2003@gmail.com>
2024-09-29 10:19:00 +0530

    form added

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,40 +1,104 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import styles from './Form.module.css'
+// import nodemailer from 'nodemailer'
+import emailjs from 'emailjs-com';
+
+import { useState } from 'react';
+
 const Form = () => {
+
+    const form = useRef();
+  const [formData, setFormData] = useState({
+    email: "",
+    message: ""
+  });
+
+  const [isValid, setIsValid] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+    
+
+    // Validate the form after each input change
+    validateForm({ ...formData, [name]: value });
+
+
+    
+  };
+
+  const validateForm = (data) => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Basic email regex pattern
+    const isEmailValid = emailPattern.test(data.email); // Check if email is valid
+    const isMessageValid = data.message.length >= 3;    // Check if message is at least 3 characters
+    setIsValid(isEmailValid && isMessageValid);
+
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (isValid) {
+      
+      emailjs.sendForm('service_h7x4oyc', 'template_xs3pguz', e.target,'UtCZOztOdliNpJ-YW')
+      .then(
+        () => {
+            alert("Form submitted successfully!");
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+      
+      // Handle form submission logic here
+    }
+    else
+    {
+      alert("Email should be valid, Description should be >=3 characters")
+    }
+    console.log(formData)
+  };
+
+  
+
   return (
     <section className={['lg:flex lg:flex-col lg:box-border w-[100%] h-[100%] rounded-3xl lg:overflow-scroll pb-2',styles['bg']].join(' ')}>
       <h1 className={['text-white text-xl font-bold  ml-4 pt-2 max-lg:text-base 2xl:text-5xl mb-0 mt-2',styles['ah1']].join(' ')}>Contact me</h1>
       <hr className='mx-3 mt-2 p-0 mb-1'/>
-      {/* 2 input fields Email, description and reset and submit buttons   */}
-      <form>
+      
+      <form onSubmit={handleSubmit} ref={form} >
         <div className='mx-3 py-2 '>
-          {/* <input className='box-border w-[100%] bg-[black] text-white rounded-full 
-          pl-2 p-1 font-bold cursor-text focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-      invalid:border-pink-500 invalid:text-pink-600
-      focus:invalid:border-pink-500 focus:invalid:ring-pink-500'  type='text' placeholder='Enter Email' /> */}
-    {/* <!-- Using form state modifiers, the classes can be identical for every input --> */}
-        <input type="email" class='font-bold block w-full px-3 py-2 cursor-text bg-black rounded-full text-sm shadow-sm placeholder-slate-400
-      focus:outline-none focus:border-sky-500
-      invalid:border-pink-500 invalid:text-pink-600
-      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
-      valid:text-sky-500
-    '  placeholder='John@gmail.com'/>
+        <input type="email" 
+        className='font-bold block w-full px-3 py-2 cursor-text
+       bg-black rounded-full text-sm shadow-sm placeholder-slate-400
+       text-white outline-none
+     '  placeholder='Enter Mail'  name="email"
+    value={formData.email}
+    onChange={handleChange}
+    required/>
         </div>
         <div className='box-border mx-3'>
         <textarea className='resize-none w-[100%] text-sm shadow-sm font-bold bg-[black]
          text-white rounded-2xl pl-3 p-2 focus:outline-none
-         ' id="w3review" rows="5" placeholder='Enter Description'>
+         ' id="w3review" rows="5" placeholder='Enter Description'
+         name="message"
+          value={formData.message}
+          onChange={handleChange}
+         >
         </textarea>
         </div>
         <div className='mx-3 my-1'>
-        <button className='bg-white text-black font-bold py-1 px-4 rounded-full text-base' type='reset'>Reset</button>
-        <button className='bg-[#0d6efd] py-1 px-4 mx-1 text-white font-bold rounded-full text-base'  type='submit' onClick={(event)=>{
-          event.preventDefault();
-          console.log("submitted");
-          }
-          }> Submit </button>
+        <button className='bg-white hover:bg-black hover:text-white text-black font-bold py-1 px-4 rounded-full text-base' type='reset'>Reset</button>
+        <button   type='submit' id='sb1'
+        className='  bg-[#0d6efd] py-1 px-4 mx-1 text-white font-bold rounded-full text-base'
+        
+         > Submit </button>
         </div>
+        {/*  */}
       </form>
     </section>
   )
--- src/components/Projects/Project_component/Project_component.js
+++ src/components/Projects/Project_component/Project_component.js
@@ -5,10 +5,9 @@ const Project_component = (props) => {
 <>
     <hr className='mx-3 p-0 mt-1 '/>
     <div className='hover:text-black rounded-md text-white hover:bg-white mx-3'>
-        
         <h1 className='  text-base font-bold  max-lg:text-sm 2xl:text-3xl ml-1 pt-1 mt-1 mb-0'>{props.title}</h1>
-        <h2 className=' lg:text-sm text-[lightgrey] text-sm font-bold ml-1 max-lg:text-xs 2xl:text-2xl mb-0'>{props.time}</h2>
-        <h2 className='text-[lightgrey] mt-1  text-sm font-bold pb-1  ml-1 max-lg:text-xs 2xl:text-2xl mb-0'>{props.tech}</h2>
+        <h2 className=' lg:text-sm  text-[lightgrey] text-sm font-bold ml-1 max-lg:text-xs 2xl:text-2xl mb-0'>{props.time}</h2>
+        <h2 className='text-[lightgrey]  mt-1  text-sm font-bold pb-1  ml-1 max-lg:text-xs 2xl:text-2xl mb-0'>{props.tech}</h2>
     </div>
     </>
   )
