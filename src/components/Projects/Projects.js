b6a86d5105378d4a251e874ee185f0fa4e2689bf
Khandyana Manindra <manindrakhandyana2003@gmail.com>
2024-10-08 07:03:35 +0530

    project section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,12 +8,14 @@ const Projects = ()=>{
        <section className={['lg:flex lg:flex-col lg:box-border w-[100%] h-[100%] rounded-3xl lg:overflow-scroll',styles['bg']].join(' ')}>
        <div className=''>
         <h1 className={['text-white text-xl font-bold  ml-4 pt-2 max-lg:text-base 2xl:text-5xl mb-0 mt-2',styles['ah1']].join(' ')}>Projects</h1>
-        <h2 className={['text-base font-bold mb-0 ml-4  max-lg:text-sm 2xl:text-3xl text-[lightgrey] lg:text-sm',styles['ah1']].join(' ')}>Projects completed : 3</h2>
+        <h2 className={['text-base font-bold mb-0 ml-4  max-lg:text-sm 2xl:text-3xl text-[lightgrey] lg:text-sm',styles['ah1']].join(' ')}>Projects completed : 4</h2>
+        <hr className='mx-3 p-0 mt-1 '/>
       </div>
       <div className='overflow-scroll mb-4 max-lg:pb-4 max-lg:h-[175px]'>
       <Link to='/Projects'>  <Project_component title="Multiple disease prediction system" time="Jan 2023 to Apr 2023" tech="Machine Learning"/></Link>
       <Link to='/Projects'>  <Project_component title="Insurance Price prediction system" time="Jan 2023 to Apr 2023" tech="Machine Learning"/></Link>
       <Link to='/Projects'>  <Project_component title="Real estate price prediction system" time="Jan 2023 to Apr 2023" tech="Machine Learning"/></Link>
+      <Link to='/Projects'>  <Project_component title="Weather Application using React" time="Jan 2024 to Apr 2024" tech="Machine Learning"/></Link>
       </div>
        </section>
         </>
